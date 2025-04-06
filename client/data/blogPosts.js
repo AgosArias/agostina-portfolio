bfaaad7b3f91027c2feccbd4a954e6e9181ee4eb
Agostina Arias <agosarias99@gmail.com>
2025-04-06 10:26:22 +0200

    add blog and project data

---
+++ client/data/blogPosts.js
@@ -0,0 +1,38 @@
+const blogPosts = [
+  {
+    title: 'Travel',
+    excerpt:
+      'Enjoying a stroll through the charming streets of Paris. Traveling allows me to explore new cultures and experiences.',
+    img: '../../images/paris.jpg',
+    link: 'post1.html',
+  },
+  {
+    title: 'Learning Languages',
+    excerpt:
+      'Taking a moment to reflect in front of the Colosseum in Rome. Learning new languages opens doors to new cultures and opportunities.',
+    img: '../../images/coliseo.jpg',
+    link: 'post2.html',
+  },
+  {
+    title: 'Drinking Mate',
+    excerpt:
+      'Enjoying a mate session, a special tradition for me. Mate is a drink that brings people together and helps them relax.',
+    img: '../../images/mate.jpg',
+    link: 'post3.html',
+  },
+  {
+    title: 'Drawing',
+    excerpt:
+      'Currently working on a new drawing. Creativity and art are important forms of expression for me.',
+    img: '../../images/dibujo.jpeg',
+    link: 'post4.html',
+  },
+  {
+    title: 'Trekking',
+    excerpt:
+      'Capturing the majesty of Fitz Roy in El Chaltén. Trekking allows me to connect with nature and challenge myself.',
+    img: '../../images/chalten.jpg',
+    link: 'post5.html',
+  },
+];
+export default blogPosts;
---
+++ client/data/projectsData.js
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    image: '../../images/projects/project1.jpg',
+    title: 'My Web Portfolio',
+    description:
+      'This web portfolio has been developed to professionally and organizedly showcase my experience, skills, and projects. Using modern technologies such as HTML, CSS, JavaScript, and Bootstrap, I have created an intuitive and visually appealing website that facilitates navigation and understanding of my capabilities. The portfolio includes detailed sections about my skills, featured projects, testimonials from colleagues and clients, and a blog to share my knowledge and experiences in the world of automation and development.',
+    shortDescription:
+      'A professional and intuitive portfolio using HTML, CSS, JS, and Bootstrap to showcase my skills, projects, and experiences.',
+    link: 'project1_details.html',
+  },
+  {
+    image: '../../images/projects/project2.jpg',
+    title: 'Financial Futures Market Analysis',
+    description:
+      'This project focused on analyzing the financial market, specifically the S&P 500 and IBEX 35 futures. Using Power BI, I developed interactive dashboards that visualize the trends and patterns of these stock indices. The analysis included a detailed review of historical price data, trading volumes, and volatility. The dashboards allow users to identify investment opportunities, evaluate market performance over different periods, and make informed decisions based on accurate and up-to-date data.',
+    shortDescription:
+      'Power BI dashboards to analyze S&P 500 and IBEX 35 futures, helping identify trends and investment opportunities.',
+    link: 'project2_details.html',
+  },
+  {
+    image: '../../images/projects/project3.jpg',
+    title: 'Project 3',
+    description: 'Brief description of Project 3.',
+    shortDescription: 'Brief overview of Project 3.',
+    link: 'project3_details.html',
+  },
+];
+
+export default projects;
