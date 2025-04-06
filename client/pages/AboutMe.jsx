77e97727cf02aa8630964ea852e4259a79b5b0b5
Agostina Arias <agosarias99@gmail.com>
2025-04-06 14:10:20 +0200

    Add pages Home, about me and notfound

---
+++ client/pages/AboutMe.jsx
@@ -0,0 +1,10 @@
+function AboutMe() {
+  return (
+    <div className="page">
+      <h2>About Me</h2>
+      <p>This is the About Me page content.</p>
+    </div>
+  );
+}
+
+export default AboutMe;
