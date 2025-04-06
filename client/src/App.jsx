1f924414c1942d4d03e621375717d07ae49050fe
Agostina Arias <agosarias99@gmail.com>
2025-04-06 14:09:38 +0200

    Change css config and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import AboutMe from '../pages/AboutMe';
+import NotFound from '../pages/NotFound';
+import Navbar from '../components/Navbar';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
