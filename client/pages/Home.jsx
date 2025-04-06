4a6a82b91fa932404a51182120cc6a7485409e29
Agostina Arias <agosarias99@gmail.com>
2025-04-06 21:47:27 +0200

    Create presentation in home

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
+import './Home.css';
+
 export default function Home() {
   return (
-    <div className="page">
-      <h2>Home</h2>
-    </div>
+    <section id="home">
+      <div className="gradient-overlay"></div>
+      <div className="max-w-4xl text-center z-10">
+        <h1>
+          Hi, I'm <span>Agostina</span>
+        </h1>
+        <h2>Full Stack Developer & RPA Specialist</h2>
+        <p>I build smart, efficient, and beautiful digital experiences💖</p>
+        <div className="buttons">
+          <div>
+            <Link to="/about">View projects</Link>
+          </div>
+          <div>
+            <Link to="/about">Contact Me</Link>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
